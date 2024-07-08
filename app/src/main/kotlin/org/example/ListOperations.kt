297ee02c76f6d88b6f737762066335fa28f951e7
Vyankatesh Nyati <vrnyati@gmail.com>
2024-07-08 22:00:50 +0530

    Refactor power methods

--- app/src/main/kotlin/org/example/ListOperations.kt
+++ app/src/main/kotlin/org/example/ListOperations.kt
@@ -1,27 +1,24 @@
 package org.example
 
+import kotlin.math.pow
+
 class ListOperations {
     companion object {
         fun getSquareValues(integerList: IntegerList): IntegerList {
-            val squareValues = IntegerList()
-
-            var current = integerList.getHead()
-
-            while (current != null) {
-                squareValues.addLast(current.value * current.value)
-                current = current.next
-            }
-
-            return squareValues
+            return getListWithPoweredValues(integerList, 2.0)
         }
 
         fun getCubeValues(integerList: IntegerList): IntegerList {
+            return getListWithPoweredValues(integerList, 3.0)
+        }
+
+        private fun getListWithPoweredValues(integerList: IntegerList, power: Double): IntegerList {
             val cubeValues = IntegerList()
 
             var current = integerList.getHead()
 
             while (current != null) {
-                cubeValues.addLast(current.value * current.value * current.value)
+                cubeValues.addLast(current.value.toDouble().pow(power).toInt())
                 current = current.next
             }
 
