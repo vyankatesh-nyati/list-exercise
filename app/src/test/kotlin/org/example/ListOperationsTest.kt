3b8cda8ff4f21a06bbbc9e1cacc668e290810136
Vyankatesh Nyati <vrnyati@gmail.com>
2024-07-09 20:59:53 +0530

    Refactor test

--- app/src/test/kotlin/org/example/ListOperationsTest.kt
+++ app/src/test/kotlin/org/example/ListOperationsTest.kt
@@ -6,13 +6,8 @@ import org.junit.jupiter.api.Test
 class ListOperationsTest {
     @Test
     fun `should return the integer list with square values for given integer list`() {
-        val integerList = IntegerList()
-        integerList.addLast(4)
-        integerList.addLast(5)
-        val expectedSquareValues = IntegerList()
-        expectedSquareValues.addLast(16)
-        expectedSquareValues.addLast(25)
-
+        val integerList = setupIntegerList(listOf(4, 5))
+        val expectedSquareValues = setupIntegerList(listOf(16, 25))
         val squareValues = ListOperations.getSquareValues(integerList)
 
         assertThat(squareValues).usingRecursiveComparison().isEqualTo(expectedSquareValues)
@@ -20,15 +15,17 @@ class ListOperationsTest {
 
     @Test
     fun `should return the integer list with cube values for given integer list`() {
-        val integerList = IntegerList()
-        integerList.addLast(4)
-        integerList.addLast(5)
-        val expectedCubeValues = IntegerList()
-        expectedCubeValues.addLast(64)
-        expectedCubeValues.addLast(125)
+        val integerList = setupIntegerList(listOf(4, 5))
+        val expectedCubeValues = setupIntegerList(listOf(64, 125))
 
         val cubeValues = ListOperations.getCubeValues(integerList)
 
         assertThat(cubeValues).usingRecursiveComparison().isEqualTo(expectedCubeValues)
+    }
+
+    private fun setupIntegerList(numbers: List<Int>): IntegerList {
+        val integerList = IntegerList()
+        numbers.forEach { integerList.addLast(it) }
+        return integerList
     }
 }
